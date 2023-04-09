c1a4b3e485721c45e6b320a5118cbb9cd263cb47
nahian255 <nahimn253@gmail.com>
2023-04-09 22:40:32 +0600

    heder part start

---
+++ src/Component/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+                <h1>text</h1>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
 const Home = () => {
     return (
         <div>
+            <Header />
             <h1>i am home</h1>
         </div>
     );
